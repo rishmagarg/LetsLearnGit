d63ebb2e1fa9959a844f2ba3aa74b6d67e0b1e24
Rishma Garg <rishmagarg0@gmail.com>
2023-06-08 16:17:25 +0530

    Multiplying two numbers

---
+++ multiply.java
@@ -0,0 +1,9 @@
+public class multiply{
+     public static void main(String[] args)
+     {
+        int a=5;
+        int b=6;
+
+        System.out.println("The sum of two numbers is:" (a*b));
+     }
+}
